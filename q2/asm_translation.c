ce06c50ebcf4b5febd2b740441acdc8195e6247e
Zidane Karim <zkarim7676@gmail.com>
2025-03-02 11:18:41 -0500

    fixing translation in 2

--- q2/asm_translation.c
+++ q2/asm_translation.c
@@ -21,15 +21,9 @@ int main() {
     int A[10]; // $s6
     // addi $t0, $s6, 4
     // A is the base address of the array A, so adding 4 to it will give the address of A[1]
-    int* t0 = A + 1;
-    // add $t1, $s6, $0
-    int* t1 = A + 0;
-    // sw $t1, 0($t0)
-    *(t0 + 0) = *t1; // sw is backwards with immediate first and register second
-    // lw $t0, 0($t0)
-    t0 = *(t0 + 0); // dangerous cast, but this is the translation of the operation. in ideal
-    // case, just store in another variable
-     // add $s0, $t1, $t0
-    f = (t0) + t1; // not meaningful since we are adding int and ptr, but this is the translation
+    A[1] = (int) &A[0];
+
+    // add $s0, $t1, $t0
+    f = (A[1]) + (int)(&A[0]); // another dangerous cast, but this is the translation of the operation.
     return 0;
 }
